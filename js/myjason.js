9f83fcf4712c4e55e1a2d7c12ddb434a2d04144d
rsobrado <rogers@thehangar.cr>
2013-08-13 19:42:20 -0600

    ejercicio 1

---
+++ js/myjason.js
@@ -0,0 +1,15 @@
+var carroJSON = {
+    tipo:           'hatchback',
+    marca:          'Toyota',
+    modelo:         'Yaris',
+    ano:            '2001',
+    inscrito:        true,
+    kilometraje:     125000,
+    duenos: ['Jorge Rojas'],
+    motor: {
+        cinlindrada: null,
+        kilometroGalon: null,
+        peso: null
+    }
+}
+console.log(carroJSON);
